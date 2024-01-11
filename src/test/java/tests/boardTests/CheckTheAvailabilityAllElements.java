8561041436163473af1c5ec4d8935cdcf1e888d8
Sofia Shevchuk <sofiashevtcuk2.04@gmail.com>
2024-01-11 16:05:49 +0200

    Added new test

--- src/test/java/tests/boardTests/CheckTheAvailabilityAllElements.java
+++ src/test/java/tests/boardTests/CheckTheAvailabilityAllElements.java
@@ -1,39 +1,72 @@
 package tests.boardTests;
 
 import api.clients.ApiBoardClient;
+import api.clients.ApiCardClient;
 import api.clients.ApiListClient;
 import api.pojo.requests.BoardBuilder;
+import api.pojo.requests.CardBuilder;
 import api.pojo.requests.ListBuilder;
+import com.codeborne.selenide.ElementsCollection;
 import io.qameta.allure.Description;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import tests.TestInit;
 import ui.pages.BoardPage;
 import ui.pages.TrelloHomePage;
+import utils.ElementUtil;
+
+import java.util.List;
 
 public class CheckTheAvailabilityAllElements extends TestInit {
     private String boardId;
-    private String listName;
+    private String listId;
+    private String cardId;
     private final BoardPage boardPage = new BoardPage();
     private final SoftAssert softAssert = new SoftAssert();
     private final BoardBuilder boardBody = BoardBuilder.builder().build();
-    private final ListBuilder listBuilder = ListBuilder.builder().build();
+    private final ListBuilder listBody = ListBuilder.builder().build();
+    private final CardBuilder cardBody = CardBuilder.builder().build();
     private final ApiListClient apiListClient = new ApiListClient(BASE_URL);
     private final ApiBoardClient apiBoardClient = new ApiBoardClient(BASE_URL);
+    private final ApiCardClient apiCardClient = new ApiCardClient(BASE_URL);
     private final TrelloHomePage trelloHomePage = new TrelloHomePage();
 
     @BeforeMethod
     public void setUp() {
         boardId = apiBoardClient.createNewBoard(boardBody, 200).getId();
-        listName = apiListClient.createNewList(listBuilder, boardId, 200).getName();
+        listId = apiListClient.createNewList(listBody, boardId, 200).getId();
+        cardId = apiCardClient.createNewCard(cardBody, listId, 200).getId();
     }
 
 
     @Test(description = "PJ2024-28")
     @Description("2.2 Check the availability and correctness of the main elements of the interface (board, lists, cards)")
-    public void checkTheAvailabilityAllElements(){
+    public void checkTheAvailabilityAllElements() {
+        refreshPage();
 
+        String boardName = boardBody.getName();
+        String listName = listBody.getName();
+        String cardName = cardBody.getName();
+        List<String> allListTitles;
+        List<String> allCardsTitles;
+
+
+        trelloHomePage.getAllBoardsFragment().specialBoardTitle(boardName).click();
+
+        ElementsCollection allCardsTitlesElements = boardPage.getBoardWorkSpaceFragment().getAllCardsTitlesInList(listName);
+        allCardsTitles = ElementUtil.getListOfStrings(allCardsTitlesElements);
+
+        ElementsCollection allListsTitlesElements = boardPage.getBoardWorkSpaceFragment().getAllListTitles();
+        allListTitles = ElementUtil.getListOfStrings(allListsTitlesElements);
+
+        softAssert.assertTrue(allCardsTitles.stream().anyMatch(genre -> genre.equals(cardName)));
+        softAssert.assertTrue(allListTitles.stream().anyMatch(genre -> genre.equals(listName)));
     }
 
+    @AfterMethod
+    private void deleteBoard() {
+        apiBoardClient.deleteExistingBoard(boardId, 200);
+    }
 }
