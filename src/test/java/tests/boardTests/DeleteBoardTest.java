aa8a34f27b46559ca9c84e549f8eb8a785c5a399
Vitalik <099626808333vvv@gmail.com>
2024-01-12 17:15:53 +0200

    PJ2024-11 finish test

--- src/test/java/tests/boardTests/DeleteBoardTest.java
+++ src/test/java/tests/boardTests/DeleteBoardTest.java
@@ -3,8 +3,6 @@ package tests.boardTests;
 import api.clients.ApiBoardClient;
 import api.pojo.requests.BoardBuilder;
 import jdk.jfr.Description;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import tests.TestInit;
@@ -15,36 +13,28 @@ import java.util.List;
 import static com.codeborne.selenide.Selenide.refresh;
 
 public class DeleteBoardTest extends TestInit {
-    private String boardId;
-    private String boardNew;
+    private String newBoardId;
     private final ApiBoardClient apiBoardClient = new ApiBoardClient(BASE_URL);
     private final SoftAssert softAssert = new SoftAssert();
     private final AllBoardsFragment allBoardsFragment = new AllBoardsFragment();
-    private final BoardBuilder firstBoardBody = BoardBuilder.builder().build();
     private final BoardBuilder secondBoardBody = BoardBuilder.builder().build();
-
-    @BeforeMethod
-    public void setUp() {
-        boardId = apiBoardClient.createNewBoard(firstBoardBody, 200).getId();
-
-    }
 
     @Test(description = "PJ2024-39")
     @Description("Delete Board")
     public void deleteBoardTest() {
-        boardNew = apiBoardClient.createNewBoard(secondBoardBody, 200).getId();
+        newBoardId= apiBoardClient.createNewBoard(secondBoardBody, 200).getId();
 
+        refresh();
         List<String> allBoardsList;
         List<String> allBoardsListAfterDeleting;
         allBoardsList = allBoardsFragment.getAllYourBoardsTitles();
-        refresh();
 
         int allBoardsCount = allBoardsList.size();
         System.out.println("Size after creation: " + allBoardsCount);
 
         softAssert.assertTrue(allBoardsCount > 0, "List is empty");
 
-        apiBoardClient.deleteExistingBoard(boardNew, 200);
+        apiBoardClient.deleteExistingBoard(newBoardId, 200);
         refresh();
 
         allBoardsListAfterDeleting = allBoardsFragment.getAllYourBoardsTitles();
@@ -54,12 +44,6 @@ public class DeleteBoardTest extends TestInit {
 
         softAssert.assertEquals(allBoardsCount - allBoardsCountAfterDeleting, 1);
         softAssert.assertAll();
-    }
-
-    @AfterMethod
-    private void shoutDown() {
-
-        apiBoardClient.deleteExistingBoard(boardId, 200);
     }
 }
 
