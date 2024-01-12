6e019b70d0e897ab8ac274d584bd9b0ab511f23d
Vitalik <099626808333vvv@gmail.com>
2024-01-12 16:57:45 +0200

    PJ2024-11 finish test

--- src/test/java/tests/boardTests/DeleteBoardTest.java
+++ src/test/java/tests/boardTests/DeleteBoardTest.java
@@ -9,37 +9,51 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import tests.TestInit;
 import ui.fragments.AllBoardsFragment;
+
 import java.util.List;
+
+import static com.codeborne.selenide.Selenide.refresh;
 
 public class DeleteBoardTest extends TestInit {
     private String boardId;
     private String boardNew;
     private final ApiBoardClient apiBoardClient = new ApiBoardClient(BASE_URL);
-    private final BoardBuilder boardBody = BoardBuilder.builder().build();
     private final SoftAssert softAssert = new SoftAssert();
     private final AllBoardsFragment allBoardsFragment = new AllBoardsFragment();
+    private final BoardBuilder firstBoardBody = BoardBuilder.builder().build();
+    private final BoardBuilder secondBoardBody = BoardBuilder.builder().build();
 
     @BeforeMethod
     public void setUp() {
+        boardId = apiBoardClient.createNewBoard(firstBoardBody, 200).getId();
 
-        boardId = apiBoardClient.createNewBoard(boardBody, 200).getId();
     }
 
     @Test(description = "PJ2024-39")
     @Description("Delete Board")
     public void deleteBoardTest() {
-        boardNew = apiBoardClient.createNewBoard(boardBody, 200).getId();
+        boardNew = apiBoardClient.createNewBoard(secondBoardBody, 200).getId();
 
         List<String> allBoardsList;
+        List<String> allBoardsListAfterDeleting;
         allBoardsList = allBoardsFragment.getAllYourBoardsTitles();
+        refresh();
+
         int allBoardsCount = allBoardsList.size();
+        System.out.println("Size after creation: " + allBoardsCount);
 
         softAssert.assertTrue(allBoardsCount > 0, "List is empty");
 
         apiBoardClient.deleteExistingBoard(boardNew, 200);
-        int allBoardsCountAfterDeleting = allBoardsList.size();
+        refresh();
+
+        allBoardsListAfterDeleting = allBoardsFragment.getAllYourBoardsTitles();
+
+        int allBoardsCountAfterDeleting = allBoardsListAfterDeleting.size();
+        System.out.println("Size after deleting: " + allBoardsCountAfterDeleting);
 
         softAssert.assertEquals(allBoardsCount - allBoardsCountAfterDeleting, 1);
+        softAssert.assertAll();
     }
 
     @AfterMethod
