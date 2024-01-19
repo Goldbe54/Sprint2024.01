5196a015dc43344c0090148c4cb85491508a1999
Goldbe54 <56392283+Goldbe54@users.noreply.github.com>
2024-01-19 12:42:08 +0200

    Update src/main/java/ui/fragments/BoardHeaderFragment.java
    Co-authored-by: ArtemKostiuktest <130027684+ArtemKostiuktest@users.noreply.github.com>

--- src/main/java/ui/fragments/BoardHeaderFragment.java
+++ src/main/java/ui/fragments/BoardHeaderFragment.java
@@ -6,7 +6,9 @@ import static com.codeborne.selenide.Condition.interactable;
 import static com.codeborne.selenide.Selenide.$x;
 
 public class BoardHeaderFragment {
+
     private String BOARD_MENU_BUTTON = ".//span[@data-testid='OverflowMenuHorizontalIcon']";
+    
     private SelenideElement rootElement() {
         return $x("//div[contains(@Class,'js-board-header')]");
     }
